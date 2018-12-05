cdb971417d9adeacb3add58b0d77095dfcc138ed
Ranjan <Vivek.Ranjan@allscripts.com>
2018-12-05 16:22:25 +0530

    added unit test project

---
+++ WebAppTestPipelineUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebAppTestPipelineUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsNotNull("value");
+        }
+    }
+}
