b038c30cde62c30baac55a461f58a42f665e14f3
Ranjan <Vivek.Ranjan@allscripts.com>
2018-12-06 14:55:02 +0530

    testing

--- WebAppTestPipelineUnitTest/UnitTest1.cs
+++ WebAppTestPipelineUnitTest/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace WebAppTestPipelineUnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            //
             Assert.IsTrue(true);     
         }
         [TestMethod]
